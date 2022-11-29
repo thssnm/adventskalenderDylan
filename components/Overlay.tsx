85c04c9c1446ba3013d67887b6324558425c21ab
thssn <theissen_matthias@hotmail.com>
2022-11-29 20:30:20 +0100

    first text

--- components/Overlay.tsx
+++ components/Overlay.tsx
@@ -24,8 +24,9 @@ export const Overlay = ({ item, visible, setVisible }: {
     }}
     >
         <div style={{ width: "60%", height: "40%", backgroundColor: "#828282", borderRadius: 12, padding: 24, overflowY: "scroll", borderColor: "white", borderWidth: 22 }}>
-            <h2 style={{ color: "white", fontFamily: "sans-serif" }}>{item.number + ". " + item.title}</h2>
-            <p style={{ color: "white", fontFamily: "sans-serif" }}>{item.text}</p>
+            <h2 style={{ color: "white", fontFamily: "sans-serif" }}>{item.number}</h2>
+           {/*  <div style={{ color: "white", fontFamily: "sans-serif" }}>{item.text}</div> */}
+            <div dangerouslySetInnerHTML={{__html: item.text}} />
         </div>
     </div>;
 };
