2a28655fde688ea3097a2b99501f3c9c3d9ac145
thssnm <46417655+thssnm@users.noreply.github.com>
2023-11-30 21:47:02 +0100

    Update numbers.ts

--- components/numbers.ts
+++ components/numbers.ts
@@ -4,7 +4,28 @@ export const numbers = [
   {
     number: "1",
     title: "title",
-    text: "test",
+    text: `Herzlich willkommen zum diesjährigen Adventskalender. Ich bin sehr froh dich dabei zu haben und möchte dich in den nächsten 24 Tagen auf eine Reise voller spannender Kaffees, Erfahrungen und neuem Wissen mitnehmen.
+Die meisten von euch sind bereits zum 2. 3. oder gar 4. Mal dabei, was mich sehr stolz macht!
+Für alle Neuen: ich bin Duje, die künstliche Intelligenz von... ok, soweit sind wir noch nicht, alle Texte wurden noch von Hand geschrieben und als Kaffeeröster seit nun zehn Jahren und Inhaber von Dylan & Harper freue ich mich euch eine Vielzahl an neuen Kaffeeerfahrungen, die auch mir während des Jahres begegnet sind, bieten zu können.
+<BR><BR>
+Bevor wir zum ersten Türchen übergehen, möchte ich diesen Auftakt nutzen und darauf hinweisen, dass ihr jederzeit per Mail oder im Ladengeschäft Kontakt mit mir aufnehmen könnt. Etwaige Fragen zu den Türchen, Kritik oder Kaufwünsche sind immer gern gesehen. Hierfür mache ich bis Weihnachten fast jeden Tag auf. Wir erweitern die Öffnungszeiten um den Donnerstag und haben somit nur noch Mittwochs Ruhetag, wobei auch dieser Richtung Weihnachten vermutlich öffnen wird.
+Was Öffnungszeiten angeht gilt wie immer: auf Google sind wir topaktuell, schaut am besten da.
+<BR><BR>
+Da nur wenige Kalender von mir gemahlen wurden, war es auch in diesem Jahr wieder eine der Aufgaben, euch die Übergänge so einfach wie möglich zu gestalten. Es wurde stets darauf geachtet, dass Kaffees mit ähnlicher Beschaffenheit aufeinanderfolgen und vor allem Bohnengrössen sich nicht ständig abwechseln.
+Das und vieles mehr ist an Gedanken in den Kalender hineingeflossen. 
+Am Ende steht dieses wundervoll handverpackte Produkt in eurem Heim (und bei manchen auf der Arbeit ;) ) und ich hoffe, dass es euch eine Freude bereiten wird!
+Die Rahmentürchen weisen auf Kaffees hin, die nicht regulär im Regal/Onlineshop stehen, sondern nur für euch geröstet wurden. 
+Ca. 30% der Röstcharge wurde für den Kalender genutzt, 10% werden wir an dem Tag hier ausschenken, sodass ca. die Hälfte übrig bleibt und ihr euch für die Feiertage damit eindecken könnt. 
+Am Tag eines jeden Türchens sollte der Kaffee seine „trinkreife“ erreicht haben und diesen Geschmackshöhepunkt hält er, unter Luftausschluss, ungefähr bis Februar. Seid also unbesorgt, falls mal ein Türchen nicht gleich verwendet wird.
+Sollte mir im Laufe der Tage etwas auffallen, kommuniziere ich mit euch über diesen digitalen Kalender.
+Ansonsten bedanke ich mich und wünsche viel Spaß :-)
+<BR><BR>
+1.<BR>
+Java - Dark<BR>
+Keine Sorge, auch wenn bei dem einen oder anderen Kaffee „Dark“ oder „Light“ steht, dann gilt das immer nur in seinem Vergleich zu sich selbst..klingt irgendwie komisch, vielleicht ist es so einfacher: der Java Dark ist nur „darker“ (dunkler) als andere Java Röstungen, die im Shop stehen oder euch vielleicht hier noch erwarten.
+Gerade in der aktuellen Charge ist der als „Dark“ verpackte Java eher eine mittelkräftige Version seiner selbst. Weit entfernt von einem klassisch italienischen Espresso, eher modern mit leichten Fruchtnoten und dennoch, gerade in Milchgetränken, kommt die Schoko-Seite durch. Pur gibt es noch ein paar Röstaromen obendrauf. 
+Für einen leichten Filterkaffee nicht meine erste Wahl, aber ich denke, dass er dennoch auch in einer Filterbrühmethode, gerade bei diesen kalten Temperaturen, sehr gefällig ist.
+Da Java ursprünglich ein Äthiopier ist, der einige Jahrzehnte auf der Insel Java verbracht hatte und nun bei uns in Mexiko wächst, müssen wir ihn trotzdem etwas feiner mahlen, weil sich die großen äthiopischen Bohnen in der Genetik und Wachstum der Pflanze dann doch durchgesetzt haben.  `,
   },
   { number: "9", title: "title", text: "" },
   { number: "17", title: "title", text: "" },
