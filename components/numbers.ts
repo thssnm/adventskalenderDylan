6305f3ebecdddc07a9cd8ea9dd53da9e9b7e4246
thssnm <46417655+thssnm@users.noreply.github.com>
2024-11-30 19:36:19 +0100

    Update numbers.ts

--- components/numbers.ts
+++ components/numbers.ts
@@ -35,28 +35,40 @@ Ich bin auf euren Eindruck gespannt!
   {
     number: "1",
     title: "title",
-    text: `Herzlich test zum diesjährigen Adventskalender. <BR><BR>Ich bin sehr froh dich dabei zu haben und möchte dich in den nächsten 24 Tagen auf eine Reise voller spannender Kaffees, Erfahrungen und neuem Wissen mitnehmen.
-Die meisten von euch sind bereits zum 2. 3. oder gar 4. Mal dabei, was mich sehr stolz macht!
-Für alle Neuen: ich bin Duje, die künstliche Intelligenz von... ok, soweit sind wir noch nicht, alle Texte wurden noch von Hand geschrieben und als Kaffeeröster seit nun zehn Jahren und Inhaber von Dylan & Harper freue ich mich euch eine Vielzahl an neuen Kaffeeerfahrungen, die auch mir während des Jahres begegnet sind, bieten zu können.
-<BR><BR>
-Bevor wir zum ersten Türchen übergehen, möchte ich diesen Auftakt nutzen und darauf hinweisen, dass ihr jederzeit per Mail oder im Ladengeschäft Kontakt mit mir aufnehmen könnt. Etwaige Fragen zu den Türchen, Kritik oder Kaufwünsche sind immer gern gesehen. Hierfür mache ich bis Weihnachten fast jeden Tag auf. Wir erweitern die Öffnungszeiten um den Donnerstag und haben somit nur noch Mittwochs Ruhetag, wobei auch dieser Richtung Weihnachten vermutlich öffnen wird.
-Was Öffnungszeiten angeht gilt wie immer: auf Google sind wir topaktuell, schaut am besten da.
-<BR><BR>
-Da nur wenige Kalender von mir gemahlen wurden, war es auch in diesem Jahr wieder eine der Aufgaben, euch die Übergänge so einfach wie möglich zu gestalten. Es wurde stets darauf geachtet, dass Kaffees mit ähnlicher Beschaffenheit aufeinanderfolgen und vor allem Bohnengrössen sich nicht ständig abwechseln.
-Das und vieles mehr ist an Gedanken in den Kalender hineingeflossen. 
-Am Ende steht dieses wundervoll handverpackte Produkt in eurem Heim (und bei manchen auf der Arbeit ;) ) und ich hoffe, dass es euch eine Freude bereiten wird!
-Die Rahmentürchen weisen auf Kaffees hin, die nicht regulär im Regal/Onlineshop stehen, sondern nur für euch geröstet wurden. 
-Ca. 30% der Röstcharge wurde für den Kalender genutzt, 10% werden wir an dem Tag hier ausschenken, sodass ca. die Hälfte übrig bleibt und ihr euch für die Feiertage damit eindecken könnt. 
-Am Tag eines jeden Türchens sollte der Kaffee seine „trinkreife“ erreicht haben und diesen Geschmackshöhepunkt hält er, unter Luftausschluss, ungefähr bis Februar. Seid also unbesorgt, falls mal ein Türchen nicht gleich verwendet wird.
-Sollte mir im Laufe der Tage etwas auffallen, kommuniziere ich mit euch über diesen digitalen Kalender.
-Ansonsten bedanke ich mich und wünsche viel Spaß :-)
-<BR><BR>
+    text: `Es ist soweit, wir starten in eine neue sensorische Reise, die den Fokus auf Kaffeevielfalt aber vor Allem auf das Vergnügen mit unserem Lieblingsgetränk legt.<BR>
+Dieses Jahr haben wir unsere Kapazität in allen Belangen auf die Probe gestellt.<BR>
+Nicht nur, dass wir mit dem 5. Adventskalender zwei Versionen haben und somit über 30 verschiedene Kaffees rösten mussten, sondern auch nach über zweitausend kleinen Tütchen, die alle per Hand verpackt wurden, kamen immer neue Interessenten dazu, sodass wir erst gestern um 16 Uhr tatsächlich dieses Projekt beendet haben..<BR>
+Nicht ganz, denn für mich beginnt jetzt der zweite Spaß! ;-)<BR><BR>
+
+In den kommenden Tagen werdet ihr hier Tag für Tag einen handgeschriebenen Text lesen können, kein ChatGPT, kein Copy Paste - ich hoffe, dass ich euch auch in diesem Jahr auf eine Reise mitnehmen kann und euch viel aus meiner Welt als Coffeologe und mittlerweile elf Jahren als Kaffeeröster zeigen, Gedanken und Erfahrungen teilen, sowie mit Zukunftsperspektiven überraschen kann.<BR>
+Wir blicken auf über 500 Jahre des Kaffeekonsums zurück, aber haben erst in den letzten Jahrzehnten vermutlich die 90% des heutigen relevanten Wissens erlangt.<BR>
+Für Fragen stehe ich jederzeit per Mail, auf unserem Social Media oder persönlich im Geschäft (Google Öffnungszeiten sind immer aktuell) zur Verfügung.<BR>
+Über Feedback freue ich mich in jedem Fall, bitte aber um Verständnis, dass ich neben dem Tagesgeschäft an unserer Kafferbar, in der Rösterei, Kursen und der Betreuung unserer Geschäftskunden, sowie Caterings, Vorträgen und Schlaf, für eine Rückmeldung ungefähr eine Woche benötige.<BR>
+Deshalb wünsche ich erstmal einen schönen ersten Advent und für alle, die neu dabei sind oder beschenkt wurden:<BR>
+ich bin Duje Dadic, Kaffeeröster von Dylan & Harper und für die kommenden 24 Tage eures Kaffeeerlebnisses zuständig :)<BR><BR>
+
+PS. Die digitalen Türchen beziehen sich ausschließlich auf die klassische Version des Kalenders.<BR>
+Für zwei Versionen jeden Tag zu schreiben wäre einfach nicht machbar, da wir unter anderem ab der zweiten Dezemberwoche bis zum 23.12. täglich geöffnet haben.<BR>
+Individuelle Fragen zu den exklusiven Türchen, bitte individuell handhaben ;)
+
+<BR><BR><BR>
+
 1.<BR>
-Java - Dark<BR>
-Keine Sorge, auch wenn bei dem einen oder anderen Kaffee „Dark“ oder „Light“ steht, dann gilt das immer nur in seinem Vergleich zu sich selbst..klingt irgendwie komisch, vielleicht ist es so einfacher: der Java Dark ist nur „darker“ (dunkler) als andere Java Röstungen, die im Shop stehen oder euch vielleicht hier noch erwarten.
-Gerade in der aktuellen Charge ist der als „Dark“ verpackte Java eher eine mittelkräftige Version seiner selbst. Weit entfernt von einem klassisch italienischen Espresso, eher modern mit leichten Fruchtnoten und dennoch, gerade in Milchgetränken, kommt die Schoko-Seite durch. Pur gibt es noch ein paar Röstaromen obendrauf. 
-Für einen leichten Filterkaffee nicht meine erste Wahl, aber ich denke, dass er dennoch auch in einer Filterbrühmethode, gerade bei diesen kalten Temperaturen, sehr gefällig ist.
-Da Java ursprünglich ein Äthiopier ist, der einige Jahrzehnte auf der Insel Java verbracht hatte und nun bei uns in Mexiko wächst, müssen wir ihn trotzdem etwas feiner mahlen, weil sich die großen äthiopischen Bohnen in der Genetik und Wachstum der Pflanze dann doch durchgesetzt haben.  `,
+Heute starten wir mit einem Kaffee, der bereits letztes Jahr in den Kalender kommen sollte. Allerdings hatten wir so viele grandiose Kaffees, sodass es einfach keinen Platz gab.<BR>
+Besonders in den letzten Jahren hat die Varietät Castillo viel an Bekanntheit gewonnen. Der Anbau erfolgt in Kolumbien, deshalb ist ein Castillo außerhalb kaum zu finden.<BR>
+Wir haben wie so oft das Glück, dass unsere Kaffeebauern selbst daran interessiert sind Vielfalt und Fortschritt auf ihren Farmen zu haben.<BR>
+Andres Quintanilla Bellucci ist nicht nur ein weit entfernter Cousin der italienischen Schönheit, er hat mit seinem Agrar-Abschluss eine super Voraussetzung für die Zukunft.<BR>
+Als unser jüngster Farmer hat er die zwei Plantagen, die seit der Oma Amalia nicht mehr beachtet wurden, übernommen.<BR>
+Sein Vater und Geschwister sind alle Juristen und folgen dem Pfad des Opas, der damals als erster Staatsanwalt El Salvadors im Bürgerkrieg ermordet wurde.<BR>
+Obwohl sein Vater bereits Vize-Präsident war und im Büro voller Stolz das Foto mit Helmut Kohl hängt, möchte Andres als Kaffeebauer seine Familie und das Land unterstützen.<BR>
+Aufgrund seines perfekten Englisch und ein wenig Latino-Crazyness verstehen wir uns sehr gut. Aktuell dauert es meist zwei Wochen für eine Antwort, da er frisch verheiratet ist ;-)<BR><BR>
+
+Der Castillo wurde, für El Salvador unüblich, als Natural verarbeitet und von mir relativ hell geröstet.<BR>
+Als Espresso kommt eine schöne balancierte Frucht in die Tasse, bei gutem Körper und leichter Bitterkeit.<BR>
+Als Filter muss ich gestehen habe ich diese Röstung noch nicht probiert, deshalb orientiere ich mich an meinem letzten Eindruck im Frühjahr mit ebenfalls gutem Körper, Pfläumchen und etwas Mandelblüte.<BR>
+Leider kann Andres momentan keine Naturals machen, da das Risiko zu hoch wäre, bei Regen und der Luftfeuchtigkeit die Ernte während der Trocknung zu verlieren.<BR>
+Wir haben also nur noch die paar Kilogramm von der Adventskalender Röstung auf Anfrage und werden kein offizielles Produkt daraus machen.
+<BR><BR>`,
   },
   {
     number: "9",
